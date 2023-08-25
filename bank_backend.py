99e54da1b5c38ec0eeb847c8211738499a70a008
kaiocesarnp <kaiocesarnp@gmail.com>
2023-08-25 11:02:01 -0300

    pysql

--- bank_backend.py
+++ bank_backend.py
@@ -1,3 +1,5 @@
+#Este código cria o arquivo 'clientes.db'
+
 import sqlite3 as sql
 
 class TransactionObject():
@@ -27,18 +29,21 @@ class TransactionObject():
 
     def persist(self):
         if TransactionObject.connected:
-            TransactionObject.commit()
+            TransactionObject.conn.commit()
             return True
         else:
             return False
 
 def initDB():
+    print("Iniciando a inicialização do banco de dados...")
     trans = TransactionObject()
     trans.connect()
 
-trans.execute("CREATE TABLE IF NOT EXISTS clientes (id INTEGER PRIMARY KEY, nome TEXT, sobrenome TEXT, email TEXT, cpf TEXT)")
-trans.persist()
-trans.disconnect()
+    trans.execute("CREATE TABLE IF NOT EXISTS clientes (id INTEGER PRIMARY KEY, nome TEXT, sobrenome TEXT, email TEXT, cpf TEXT)")
+    trans.persist()
+    trans.disconnect()
+
+    print("Banco de dados inicializado com sucesso.")
 
 def insert(nome, sobrenome, email, cpf):
     trans = TransactionObject()
@@ -59,10 +64,10 @@ def search(nome = "", sobrenome = "", email = "", cpf = ""):
     trans = TransactionObject()
     trans.connect()
 
-trans.execute("SELECT * FROM clientes WHERE nome = ? or sobrenome = ? or email = ? or cpf = ?", (nome, sobrenome, email, cpf))
-rows = trans.ferchall()
-trans.diconnect()
-return rows
+    trans.execute("SELECT * FROM clientes WHERE nome = ? or sobrenome = ? or email = ? or cpf = ?", (nome, sobrenome, email, cpf))
+    rows = trans.fetchall()
+    trans.disconnect()
+    return rows
 
 def delete(id):
     trans = TransactionObject()
@@ -75,37 +80,39 @@ def update(id, nome, sobrenome, email, cpf):
     trans = TransactionObject()
     trans.connect()
 
-trans.execute("UPDATE clientes SET nome = ?, sobrenome = ?, email = ?, cpf = ? WHERE id = ?", (nome, sobrenome, email, cpf, id))
-trans.persist()
-trans.disconnect()
+    trans.execute("UPDATE clientes SET nome = ?, sobrenome = ?, email = ?, cpf = ? WHERE id = ?", (nome, sobrenome, email, cpf, id))
+    trans.persist()
+    trans.disconnect()
 
 initDB()
-        
 
 
+#A classe TransationObject() será a classe que realizará as operações e a conexão com o banco de dados.
+#Analisando as suas funções:
 
+#Connected
+#É a função que realiza a conexão propriamente dita com o banco de dados.
 
+#Disconnect
+#Encerra a conexão com o banco de dados.
 
+#Execute
+#Como o próprio nome diz, essa função executa um comando no banco de dados recebendo parâmetros: Self, Sql, Parms, Fetchall () e Persist()
 
+#---
 
+#Sobre os parâmetros da função Execute, veja os conceitos:
+#Self
+#Referencia o próprio objeto.
 
+#Sql
+#Comando SQL que deve ser executado.
 
+#Parms
+#Vetor que contém os parâmetros do comando SQL e pode ser omitido.
 
+#Fetchall()
+#Obtém os valores de entrada do comando SELECT.
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-        
+#Persist()
+#Faz o commit das operações.
